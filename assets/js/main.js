dd36861dd56ddeb675a650decc22b18034f42773
João Abrantes <jpiabrantes@gmail.com>
2019-04-08 17:33:13 +0100

    virtual nations page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,147 +1,83 @@
-(function($){
-	$(window).load(function(){
+/*
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-		// INITIALIZE ANIMSITION
-		if($(".animsition").length){
-			$(".animsition").animsition({
-				inClass               :   'fade-in-up-sm',
-				outClass              :   'fade-out-up-sm',
-				inDuration            :    1100,
-				outDuration           :    800,
-				linkElement           :   '.animsition-link',
-				loading               :    true,
-				loadingParentElement  :   'body',
-				unSupportCss          : [ 'animation-duration',
-										  '-webkit-animation-duration',
-										  '-o-animation-duration'
-										],
-				overlay               :   false,
-				overlayClass          :   'animsition-overlay-slie',
-				overlayParentElement  :   'body'
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#page-wrapper'),
+		$banner = $('#banner'),
+		$header = $('#header');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
+
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
 			});
+
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
+
 		}
 
-		// INPUTS EVENTS
-		$('.input_1 input, .textarea_1 textarea').on('focus', function() {
-			$(this).next("span").addClass("active");
-		});
-		$('.input_1 input, .textarea_1 textarea').on('blur', function() {
-			if($(this).val() === ""){
-				$(this).next("span").removeClass("active");
-			}
-		});
+	// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				speed: 1500,
+				offset: $header.outerHeight()
+			});
 
+	// Menu.
+		$('#menu')
+			.append('<a href="#menu" class="close"></a>')
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'is-menu-visible'
+			});
 
-		// TABS
-		$(".bottom-line").css({
-			width : $(".tab nav a").first().width() + 20 + "px" // 20 = element's padding * 2
-		});
-		var _current_index = 0;
-		$(".tab nav a").on('click', function(){
-			e.preventDefault();
-			// tab navigation styles
-			var _this = $(this);
-			var _index = _this.index();
-			if(_current_index !== _index){
-				$(".tab nav a").each(function(){
-					if($(this).hasClass("current")) $(this).removeClass("current");
-				});
-				_this.addClass("current");
-				$(".bottom-line").css({
-					left : _this.offset().left - _this.parent().offset().left + "px",
-					width : _this.width() + 20 + "px" // 20 = element's padding * 2
-				});
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
 
-				// show tab content
-				$(".tab_single.shown").fadeOut(200);
-				setTimeout(function(){
-					$(".tab_single").eq(_index).fadeIn(200);
-					$(".tab_single").eq(_index).addClass("shown");
-				},200);
+			$window.on('resize', function() { $window.trigger('scroll'); });
 
-				_current_index = _index;
-			}
-		});
+			$banner.scrollex({
+				bottom:		$header.outerHeight() + 1,
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
 
-		// NAVBAR
-		var _link = $("nav.desktop-nav ul.first-level").children("li");
-		var shown = false;
-		// show navbar
-		$(".menu-icon").on( 'click', function(){
-			var _this = $(this);
-			$("nav.mobile-nav").slideToggle(200);
-			if(!shown){
-				_this.children("div").css("width","30px");
-				shown = true;
-			}else{
-				_this.children("div").first().css("width","30px");
-				_this.children("div").eq(1).css("width","15px");
-				_this.children("div").eq(2).css("width","20px");
-				shown = false;
-			}
-		});
+		}
 
-
-
-		 // Contact form
-  		var form = $('#main-contact-form');
-  			form.submit(function(event){
-    			event.preventDefault();
-    			var form_status = $('<div class="form_status"></div>');
-    			$.ajax({
-      				url: $(this).attr('action'),
-      					beforeSend: function(){
-        				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
-      				}
-    			}).done(function(data){
-      				form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
-    			});
-  			});
-
-
-
-		// dropdown - desktop
-		_link.on('hover', function(e) {
-			e.preventDefault();
-			var _this = $(this);
-			if(_this.children("ul.second-level").html() !== undefined){
-				if(e.type === "mouseenter"){
-					_this.children("ul.second-level").slideDown(200);
-				}else{
-					_this.children("ul.second-level").slideUp(200);
-				}
-			}
-		});
-
-		$("#owl-demo").owlCarousel({
-
-		 autoPlay: 3000, //Set AutoPlay to 3 seconds
-
-		 items : 4,
-		 itemsDesktop : [1199,5],
-		 itemsDesktopSmall : [979,3]
-
- 		});
-
-		// dropdown - mobile
-		$("nav.mobile-nav").html($("nav.desktop-nav").html()); // set navbar
-
-		var mobile_link = $("nav.mobile-nav ul.first-level").children("li");
-		mobile_link.children("a").on( 'click', function(e){
-			var _this = $(this);
-			var submenu_exists = (_this.next("ul.second-level").html() === undefined) ? false : true;
-			if(submenu_exists){
-				e.preventDefault();
-				$(".down").slideUp(200);
-				if(_this.next("ul.second-level").hasClass("down")){
-					_this.next("ul.second-level").removeClass("down");
-				}else{
-					$(".down").removeClass("down");
-					_this.next("ul.second-level").slideDown(200);
-					_this.next("ul.second-level").addClass("down");
-				}
-			}
-		});
-
-	});
 })(jQuery);
